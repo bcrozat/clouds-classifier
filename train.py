01f81ab97eac3568ca3e49888486f0245fb78574
bcrozat <baptiste.crozat@pm.me>
2025-05-30 16:43:28 +0200

    added checkpoints + earlystopping

--- train.py
+++ train.py
@@ -1,18 +1,25 @@
-# TODO: install & use lightning for training
-
 # Import dependencies
-import argparse # Allows to provide input parameters (parse arguments) from the command line instead of hardcoding them
-import time
 from tqdm.auto import tqdm
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+import argparse # Allows to provide input parameters (parse arguments) from the command line instead of hardcoding them
+import lightning as L
+import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 # Import custom modules
 from model import LitCloudNet
-from dataset import dataloader_train, dataloader_test
-#from utils import save_model, save_acc_plot, save_loss_plot
-import lightning as L
+from dataset import train_dataloader, test_dataloader
+
+# Start timer
+start_time = time.time()
+
+# Create timestamp
+timestamp = time.strftime('%Y-%m-%d-%H%M%S', time.localtime(start_time))
+
+# Print start message
+print('# Training started')
 
 # Set up argument parser
 parser = argparse.ArgumentParser() # Initialize argument parser
@@ -25,17 +32,29 @@ args = vars(parser.parse_args())
 # Parameters
 learning_rate = 1e-3
 epochs = args['epochs']
-model = LitCloudNet()
 tag = args['tag']
-print(model)
-print(f'Tag: {tag}')
 
 # Initialize the model
-
+model = LitCloudNet()
+print(f'Model: {model}')
 
 # Train the model (using Lightning)
-trainer = L.Trainer(limit_train_batches=100,
+trainer = L.Trainer(default_root_dir='checkpoints',
+                    limit_train_batches=100,
                     max_epochs=1,
-                    accelerator='gpu'
+                    accelerator='gpu',
+                    callbacks=[EarlyStopping(monitor='acc_loss', mode='max', patience=3)] # Early stopping after 3 iterations without improvement
                     )
-trainer.fit(model=model, train_dataloaders=train_loader)
+trainer.fit(model=model, train_dataloaders=train_dataloader)
+
+# Print log message
+print('# Training complete!')
+
+# Print log message
+print('# Testing started!')
+
+# Test the model
+trainer.test(model=model, train_dataloaders=test_dataloader)
+
+# Print log message
+print('# Testing complete!')
