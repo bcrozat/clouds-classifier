83f71b2f4e1d5825014bb538a57b50a9b33c696e
bcrozat <baptiste.crozat@pm.me>
2025-05-30 16:37:34 +0200

    added train & test (& val) steps

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ num_classes = 7
 class LitCloudNet(L.LightningModule):
     def __init__(self):
         super().__init__()
+        self.save_hyperparameters()
         self.feature_extractor = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.ELU(),
@@ -28,9 +29,24 @@ class LitCloudNet(L.LightningModule):
         z = self.feature_extractor(x)
         x_hat = self.classifier(z)
         loss = nn.BCELoss(x_hat, x) # nn.CrossEntropyLoss() for multi-class classification, nn.BCELoss() for binary classification
-        # Logging to TensorBoard (if installed) by default
-        self.log("train_loss", loss)
+        self.log('train_loss', loss) # Logging to TensorBoard
         return loss
+
+    def test_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self.feature_extractor(x)
+        x_hat = self.classifier(z)
+        test_loss = nn.BCELoss(x_hat, x)
+        self.log('test_loss', test_loss)
+
+    # def validation_step(self, batch, batch_idx):
+    #     x, _ = batch
+    #     x = x.view(x.size(0), -1)
+    #     z = self.feature_extractor(x)
+    #     x_hat = self.classifier(z)
+    #     val_loss = nn.BCELoss(x_hat, x)
+    #     self.log('val_loss', val_loss)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=learning_rate)
