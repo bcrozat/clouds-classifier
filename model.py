163d0b12cd63c7767678aba0910501e96cfe3c06
bcrozat <baptiste.crozat@pm.me>
2025-06-02 18:10:43 +0200

    changed to val_step

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ class LitCloudNet(L.LightningModule):
         super().__init__()
         self.save_hyperparameters()
         self.train_acc = Accuracy(task='multiclass', num_classes=num_classes)
-        self.test_acc = Accuracy(task='multiclass', num_classes=num_classes)
+        self.val_acc = Accuracy(task='multiclass', num_classes=num_classes)
         self.feature_extractor = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.ELU(),
@@ -38,23 +38,15 @@ class LitCloudNet(L.LightningModule):
         self.log_dict({'train_loss': train_loss, 'train_acc': self.train_acc}, prog_bar=True) # Logging to TensorBoard
         return train_loss
 
-    def test_step(self, batch, batch_idx):
+    def val_step(self, batch, batch_idx):
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.feature_extractor(x)
         x_hat = self.classifier(z)
-        test_loss = nn.BCELoss(x_hat, x)
+        val_loss = nn.BCELoss(x_hat, x)
         preds = torch.argmax(x_hat, dim=1)
-        self.train_acc.update(preds, y)
-        self.log_dict({'test_loss': test_loss, 'train_acc': self.test_acc}, prog_bar=True)
-
-    # def validation_step(self, batch, batch_idx):
-    #     x, _ = batch
-    #     x = x.view(x.size(0), -1)
-    #     z = self.feature_extractor(x)
-    #     x_hat = self.classifier(z)
-    #     val_loss = nn.BCELoss(x_hat, x)
-    #     self.log('val_loss', val_loss)
+        self.val_acc.update(preds, y)
+        self.log_dict({'val_loss': val_loss, 'val_acc': self.val_acc}, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=learning_rate)
