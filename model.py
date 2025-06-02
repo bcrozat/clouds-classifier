12f548bb434ea84a5bc414ae0c772c8a03104cb7
bcrozat <baptiste.crozat@pm.me>
2025-06-02 18:35:32 +0200

    updated criterion + metrics

--- model.py
+++ model.py
@@ -14,8 +14,7 @@ class LitCloudNet(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.save_hyperparameters()
-        self.train_acc = Accuracy(task='multiclass', num_classes=num_classes)
-        self.val_acc = Accuracy(task='multiclass', num_classes=num_classes)
+        self.acc = Accuracy(task='multiclass', num_classes=num_classes)
         self.feature_extractor = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.ELU(),
@@ -26,27 +25,30 @@ class LitCloudNet(L.LightningModule):
             nn.Flatten(),
         )
         self.classifier = nn.Linear(64 * 16 * 16, num_classes)
+        self.criterion = nn.CrossEntropyLoss()
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        x = x.view(x.size(0), -1)
+        x = x.float()
         z = self.feature_extractor(x)
         x_hat = self.classifier(z)
-        train_loss = nn.BCELoss(x_hat, x) # nn.CrossEntropyLoss() for multi-class classification, nn.BCELoss() for binary classification
+        train_loss = self.criterion(x_hat, x) # nn.CrossEntropyLoss() for multi-class classification, nn.BCELoss() for binary classification
+        train_acc = self.acc()
         preds = torch.argmax(x_hat, dim=1)
-        self.train_acc.update(preds, y)
-        self.log_dict({'train_loss': train_loss, 'train_acc': self.train_acc}, prog_bar=True) # Logging to TensorBoard
+        train_acc.update(preds, y)
+        self.log_dict({'train_loss': train_loss, 'train_acc': self.train_acc.compute()}, prog_bar=True) # Logging to TensorBoard
         return train_loss
 
-    def val_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx):
         x, y = batch
-        x = x.view(x.size(0), -1)
+        x = x.float()
         z = self.feature_extractor(x)
         x_hat = self.classifier(z)
-        val_loss = nn.BCELoss(x_hat, x)
+        val_loss = self.criterion(x_hat, x)
+        val_acc = self.acc()
         preds = torch.argmax(x_hat, dim=1)
-        self.val_acc.update(preds, y)
-        self.log_dict({'val_loss': val_loss, 'val_acc': self.val_acc}, prog_bar=True)
+        val_acc.update(preds, y)
+        self.log_dict({'val_loss': val_loss, 'val_acc': self.val_acc.compute()}, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=learning_rate)
